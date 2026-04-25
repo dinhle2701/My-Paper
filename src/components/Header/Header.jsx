870b4e059a78f2d7fa1e9e9a1fa5965c1fc31f75
dinh-letat <dinhprococoa@gmail.com>
2026-04-25 09:45:33 +0700

    color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 import documents from '@/data/documents'
 
 const Header = () => {
+    const doc = documents[0]
+
     return (
-        <div>
-            <h1>{doc.name}</h1>
-            <p>{doc.author}</p>
-            <p>{doc.venue} - {doc.year}</p>
+        <div className='text-black text-center'>
+            <div className="title">
+
+            </div>
+
+            <div className="info">
+                <div className="author">
+
+                </div>
+
+                <div className="educate">
+
+                </div>
+
+                <div className="link-related">
+                    <button className='bg-black text-white border-6 rounded-4xl px-5 py-2'>Paper</button>
+                    <button className='bg-black text-white border-6 rounded-4xl px-5 py-2'>arXiv</button>
+                    <button className='bg-black text-white border-6 rounded-4xl px-5 py-2'>Video</button>
+                    <button className='bg-black text-white border-6 rounded-4xl px-5 py-2'>Data</button>
+                </div>
+            </div>
+            <div className="comment">
+                <span>Accepted by NeurIPS 2023 Datasets and Benchmarks Track</span>
+            </div>
         </div>
     )
 }
--- src/components/License/License.jsx
+++ src/components/License/License.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const License = () => {
     return (
-        <div>
+        <div className='text-black '>
             <h2>MM-Fi is released under the CC BY-NC 4.0.</h2>
         </div>
     )
